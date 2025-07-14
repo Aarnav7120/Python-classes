fca160f5312cad137b9ec5adcde8785573796407
Aarnav7120 <aarnavkumarorig@gmail.com>
2025-07-14 09:45:00 +0200

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,23 @@
+"""Welcome to the world of coding
+Outline:
+Write a program for print command.
+
+print("welcome")
+print(10)"""
+
+"""Understanding the variables
+Outline:
+Write a program to create different variables to store various values.
+myvariable="Aarnav"
+print(myvariable)
+x="John"
+print(x)
+z=(100)
+print(z)"""
+
+"""Keywords
+Outline:
+Write a program to print all the keywords"""
+
+import keyword
+print(keyword.kwlist)
