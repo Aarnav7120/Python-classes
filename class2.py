7fbe22ae40ff7b1096073da4f7bb392393139af0
Aarnav7120 <aarnavkumarorig@gmail.com>
2025-07-15 18:34:54 +0200

    Add files via upload

--- class2.py
+++ class2.py
@@ -19,5 +19,20 @@ print(z)"""
 Outline:
 Write a program to print all the keywords"""
 
-import keyword
-print(keyword.kwlist)
+"""import keyword
+print(keyword.kwlist)"""
+
+a="December7th"
+print(a)
+
+r="August20th"
+print(r)
+
+v="November7th"
+print(v)
+
+n="August2nd"
+print(n)
+
+c="December24th"
+print(c)
