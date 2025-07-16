bcb7d3366875428e6d643a9a2a37b7370df24283
Aarnav7120 <aarnavkumarorig@gmail.com>
2025-07-16 11:12:21 +0200

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,39 @@
+'''Data types
+Outline:
+Write a program to find what type of datatype the given variable is?'''
+
+"""first="Aarnav"
+print(type(first))
+
+second=(4)
+print(type(second))
+
+third=(3.2)
+print(type(third))
+
+fourth=(True)
+print(type(fourth))"""
+
+"""Typecasting
+Outline:
+Write a program to change the datatype of given variables?"""
+
+"""a=(6.5)
+print(type(a))
+
+b=int(a)
+print(type(b))"""
+
+"""String operation
+Outline:
+Write a program to print the “CODINGAL” word in a reverse direction."""
+
+a="Mobile Phone"
+print(a[2:5])
+
+print(a[0::10])
+
+print(a.upper())
+
+b="Codingal"
+print(b[::-1])
