92f629753fbee00c3708afb3b605e82fa6457a07
Aarnav7120 <aarnavkumarorig@gmail.com>
2025-08-31 16:09:50 +0200

    Add files via upload

---
+++ class10.py
@@ -0,0 +1,32 @@
+"""for i in range (20, 41, 2):
+    print(i)
+
+name = ("Aarnav")
+for i in name:
+    print(i)"""
+
+"""Outline:
+Write a program to calculate the sum of whole numbers."""
+
+"""number = int(input("Enter the final value: "))
+sum = 0
+for i in range(1, number + 1):
+    sum = sum + i
+print(sum)"""
+
+"""Outline:
+Write a program to reverse the string entered by the user."""
+
+"""name = ("Harika")
+reverse = ""
+for i in name:
+    reverse = i + reverse
+print(reverse)"""
+
+"""reverse order
+Outline:
+Write a program to print the numbers in reverse order beginning from the number
+ entered by the user."""
+number = int(input("enter the number:"))
+for i in range(number, 0, -2 ):
+    print(i)
