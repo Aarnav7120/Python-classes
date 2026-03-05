f65969b2d8678c7f35582cca7b01e3f830ad0505
Aarnav7120 <aarnavkumarorig@gmail.com>
2026-03-05 19:58:57 +0100

    Add files via upload

---
+++ class30.py
@@ -0,0 +1,132 @@
+# Import necessary Modules
+"""from abc import ABC, abstractmethod
+
+class Absclass(ABC):
+
+    def print(self,x):
+        print("Passed value: ", x)
+
+    def task(self):
+        print("We are inside Absclass task")
+
+class test_class(Absclass):
+    def task(self):
+        print("we are inside test_class task")
+
+    @abstractmethod
+    def task(self):
+        print("We are inside Absclass task")
+
+
+class test_class(Absclass):
+    def task(self):
+        print("We are inside test_class task")
+
+test_obj = test_class()
+test_obj.task()
+test_obj.print(100)"""
+
+"""from abc import ABC, abstractmethod
+class Animal(ABC):
+
+
+    def move(self):
+        pass
+class Human(Animal):
+
+    def move(self):
+        print("I can walk and run")
+
+class Snake(Animal):
+
+
+    def move(self):
+        print("I can crawl")
+
+class Dog(Animal):
+
+    def move(self):
+        print("I can bark")
+
+
+class Lion(Animal):
+
+    def move(self):
+        print("I can roar")
+
+
+R = Human()
+R.move()
+
+K = Snake()
+K.move()
+
+R = Dog()
+R.move()
+
+
+K = Lion()
+K.move"""
+
+"""class India():
+    def capital(self):
+        print("New Dehli is the capital of India.")
+
+    def language(self):
+        print("Hindi is the most idely spoken language of India.")
+
+    def type(self):
+        print("India is a developing country")
+
+
+class USA():
+    def capital(self):
+        print("Washington, D.C. is the capital of USA.")
+
+    def language(self):
+        print("English is the primary language of USA.")
+
+    def type(self):
+        print("USA is a developed country")
+
+
+obj_ind = India()
+obj_usa = USA()
+
+for country in (obj_ind, obj_usa):
+    country.capital()
+    country.language()
+    country.type()"""
+
+class Cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print("Meow")
+
+    def make_sound(self):
+        print("Meow")
+
+class Dog:
+    def __init__(self, name, age ):
+        self.name = name
+        self.age = age
+    
+    def info(self):
+        print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
+
+    def make_sound(self):
+        print("Bark")
+
+
+
+cat1 = Cat("Kitty", 2.5)
+dog1 = Dog("Fluffy", 4)
+
+for animal in (cat1, dog1):
+    animal.make_sound()
+    animal.info()
+    animal.make_sound()
+
