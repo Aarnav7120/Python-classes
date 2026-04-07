152fa2554b3b6de81d391d80fb48be6f25ff135b
Aarnav7120 <aarnavkumarorig@gmail.com>
2026-04-07 11:35:11 +0200

    Add files via upload

--- class4.py
+++ class4.py
@@ -1,49 +1,69 @@
-"""Average
-Outline:
-There are five trees in Jack's front yard. He checks each tree to find out how tall it is
- in inches and writes the height on a sheet of paper. Jack's list: 98, 94, 41, 96, and 11.
-   What is the average height of a tree in Jack's front yard?"""
+import pygame
+import random
 
-"""tree1=int(input("enter the height of tree1: "))
-tree2=int(input("enter the height of tree2: "))
-tree3=int(input("enter the height of tree3: "))
-tree4=int(input("enter the height of tree4: "))
-tree5=int(input("enter the height of tree5: "))
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 400
+MOVEMENT_SPEED = 5
+FONT_SIZE = 72
 
-average=(tree1+tree2+tree3+tree4+tree5)/5
-print("the average of all the trees are ",average)"""
+pygame.init()
 
-"""Percentage calculation
-Outline:
-Raj scored 40, 70, 50 and 60 out of 100 in maths, science, Hindi and English. Find the percentage he got?"""
+background_image = pygame.transform.scale(pygame.image.load("bg.png"), (SCREEN_WIDTH, SCREEN_HEIGHT))
+font = pygame.font.SysFont("Times New Roman", FONT_SIZE)
 
-"""subject1=int(input("enter the marks of subject1: "))
-subject2=int(input("enter the marks of subject2: "))
-subject3=int(input("enter the marks of subject3: "))
-subject4=int(input("enter the marks of subject4: "))
-sum=subject1+subject2+subject3+subject4
-percentage=(sum/400)*100
-print("the percentage of all subjects are", percentage)"""
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(pygame.Color("dodgerblue"))
+        pygame.draw.rect(self.image, color, pygame.Rect(0, 0, width, height))
+        self.rect = self.image.get_rect()
+       
+    def move(self, x_change, y_change):
+        self.rect.x = max
+        (min(self.rect.x + x_change, SCREEN_WIDTH - self.rect.width), 0)
+        self.rect.y = max
+        (min(self.rect.y + y_change, SCREEN_HEIGHT - self.rect.height), 0)
 
-"""Count the notes
-Outline:
-Write a program to calculate the number of notes in the given amount?"""
 
-"""amount=int(input("Please enter the amount: "))
-note10=amount/10
-note50=amount/50
-note100=amount/100
-note500=amount/500
-print("requierd 10 rs notes are ",note10)
-print("requierd 50 rs notes are ",note50)
-print("requierd 100 rs notes are ",note100)
-print("requierd 500 rs notes are ",note500)"""
+screen  = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Sprite Collision")
+all_sprites = pygame.sprite.Group()
 
-import math
-number = float(input("Please enter a number: "))
+sprite1 = Sprite(pygame.Color('black'), 20, 30)
+sprite1.rect.x, sprite1.rect.y = random.randint(0, SCREEN_WIDTH - sprite1.rect.width), random.randint(0, SCREEN_HEIGHT - sprite1.rect.height)
+all_sprites.add(sprite1)
 
-if number < 0:
-        print("Sorry, square root of a negative number is not real.")
-else:
-        square_root = math.sqrt(number)
-        print(f"The square root of {number} is {square_root}")
+sprite2 = Sprite(pygame.Color("red"), 20, 30)
+sprite2.rect.x, sprite2.rect.y = random.randint(0, SCREEN_WIDTH - sprite2.rect.width), random.randint(0, SCREEN_HEIGHT - sprite2.rect.height)
+all_sprites.add(sprite2)
+
+running, won = True, False
+clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            running = False
+
+    if not won:
+        keys = pygame.key.get_pressed()
+        x_change = (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * MOVEMENT_SPEED
+        y_change = (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * MOVEMENT_SPEED
+        sprite1.move(x_change, y_change)
+
+        if sprite1.rect.colliderect(sprite2.rect):
+            won = True
+
+
+    screen.blit(background_image, (0, 0))
+
+    all_sprites.draw(screen)
+
+    if won:
+        win_text = font.render("You Win!", True, pygame.Color("black"))
+        screen.blit(win_text, ((SCREEN_WIDTH - win_text.get_width()) // 2, (SCREEN_HEIGHT - win_text.get_height()) // 2))
+    pygame.display.flip()
+    clock.tick(90)
+
+pygame.quit()
+
+            
