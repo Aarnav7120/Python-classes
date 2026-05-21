ad32a6616ed09fe4afe97057424ce5ce9d898e98
Aarnav7120 <aarnavkumarorig@gmail.com>
2026-05-21 19:38:40 +0200

    Add files via upload

--- class3.py
+++ class3.py
@@ -1,81 +1,38 @@
-import pygame
-import random
-
-pygame.init()
-
-SPRITE_COLOR_CHANGE_EVENT = pygame.USEREVENT + 1
-BACKGROUND_COLOR_CHANGE_EVENT = pygame.USEREVENT + 2
+"""from tkinter import *
 
 
-Yellow = pygame.Color('yellow')
-Magenta = pygame.Color('magenta')
-orange = pygame.Color('orange')
-white = pygame.Color('white')
+window = Tk()
+window.title("Event Handler")
+window.geometry('400x400')
+
+def handle_keypress(event):
+    print(event.char)
+
+window.bind("<Key>", handle_keypress)
+
+def handle_click(event):
+    print("\nThe button was clicked")
+
+button = Button(text="Click me!")
+button.pack()
+
+button.bind("<Button-1>", handle_click) 
+
+window.mainloop()"""
 
 
-class Sprite(pygame.sprite.Sprite):
-    def __init__(self, color, width, height):
-        super().__init__()
-        self.image = pygame.Surface((width, height))
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
-        self.velocity = [random.randint(-1, 1), random.randint(-1, 1)]
 
 
-    def update(self):
-        self.rect.move_ip(self.velocity)
+from tkinter import *
+from tkinter import messagebox
 
-        boundary_hit = False
+root = Tk()
+root.geometry('400x400')
 
-        if self.rect.left < 0 or self.rect.right > 500:
-            self.velocity[0] = -self.velocity[0]
-            boundary_hit = True
+def msg():
+    messagebox.showwarning("Alert", "Stop! virus Found.")
 
-        if self.rect.top < 0 or self.rect.bottom > 400:
-            self.velocity[1] = -self.velocity[1]
-            boundary_hit = True
+button = Button(root, text="Scan for virus", command=msg)
+button.place(x=40, y=80)
 
-        if boundary_hit:
-            pygame.event.post(pygame.event.Event(SPRITE_COLOR_CHANGE_EVENT))
-            pygame.event.post(pygame.event.Event(BACKGROUND_COLOR_CHANGE_EVENT))
-
-    def change_color(self):
-        self.image.fill(random.choice([Yellow, Magenta, orange, white]))
-
-def change_background_color():
-    global bg_color
-    bg_color = random.choice([Yellow, Magenta, orange, white])
-
-all_sprites_list = pygame.sprite.Group()
-sp1 = Sprite(white, 20, 30)
-sp1.rect.x = random.randint(0, 480)
-sp1.rect.y = random.randint(0, 370)
-all_sprites_list.add(sp1)
-
-screen = pygame.display.set_mode((500, 400))
-pygame.display.set_caption("Sprite Collision Example")
-bg_color = Yellow
-
-screen.fill(bg_color)
-
-exit = False
-clock = pygame.time.Clock()
-while not exit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit = True
-        elif event.type == SPRITE_COLOR_CHANGE_EVENT:
-            sp1.change_color()
-        elif event.type == BACKGROUND_COLOR_CHANGE_EVENT:
-            change_background_color()
-
-    all_sprites_list.update()
-
-    screen.fill(bg_color)
-    all_sprites_list.draw(screen)
-
-    pygame.display.flip()
-    clock.tick(240)
-
-pygame.quit()
-
+root.mainloop()
